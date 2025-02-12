c728eae63f617bad1952490634ed1945e44b5dbc
rmaree <raphael.maree@ulg.ac.be>
2025-02-12 08:57:03 +0100

    feat: use config.js file for configuration variables

---
+++ config.js
@@ -0,0 +1,5 @@
+ // the real-time API end-point
+const RT_API_URL = ''; // the real-time API end-point
+const RT_REFRESH_RATE = 10000; // refresh real-time traffic data every 10 seconds
+// the tile server API key
+const TILE_API_KEY = ''; // the tile server API key
