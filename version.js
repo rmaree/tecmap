9219707ff5dcb5b6150b2b96eeb5a78073a9f896
Shogun Web <info@shogunweb.be>
2026-05-15 20:09:36 +0000

    chore: update TECMAP_VERSION to reflect the latest build number

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 (function (globalScope) {
-  globalScope.TECMAP_VERSION = "20260515-2205-6548";
+  globalScope.TECMAP_VERSION = "20260515-2209-6548";
 })(typeof self !== "undefined" ? self : window);
